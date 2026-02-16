53faf72ffdba7aaca0ad49482a96c1da4cc02605
Bluscream <Bluscream@users.noreply.github.com>
2026-02-16 02:53:41 +0100

    Refactor: Adhered to Gold Standard conventions

--- index.tsx
+++ index.tsx
@@ -10,15 +10,8 @@ import { findAssociatedTextChannel } from "./utils/channels";
 import { PreviousVoiceState } from "./types/PreviousVoiceState";
 import { VoiceStateChangeEvent } from "./types/VoiceStateChangeEvent";
 // endregion Imports
-// region PluginInfo
-export const pluginInfo = {
-    id: "voiceChannelLog",
-    name: "VoiceChannelLog",
-    description: "Logs voice channel joins and leaves to the associated text channel",
-    color: "#7289da",
-    authors: [{ name: "Bluscream", id: 467777925790564352n }, { name: "Cursor.AI", id: 0n }],
-};
-// endregion PluginInfo
+import { pluginInfo } from "./info";
+export { pluginInfo };
 // region Utils
 function getTypeAndChannelId(
     { channelId, oldChannelId }: VoiceStateChangeEvent,
---
+++ info.ts
@@ -0,0 +1,7 @@
+export const pluginInfo = {
+    id: "voiceChannelLog",
+    name: "VoiceChannelLog",
+    description: "Logs voice channel joins and leaves to the associated text channel",
+    color: "#7289da",
+    authors: [{ name: "Bluscream", id: 467777925790564352n }, { name: "Cursor.AI", id: 0n }],
+};
