ea7795844874ce8b98da282701feaa3734838334
Bluscream <Bluscream@users.noreply.github.com>
2026-02-16 02:59:15 +0100

    20

--- info.ts
+++ info.ts
@@ -1,6 +1,6 @@
 export const pluginInfo = {
-    id: "voiceChannelLog",
-    name: "VoiceChannelLog",
+    id: "voiceChannelChatLog",
+    name: "VoiceChannelChatLog",
     description: "Logs voice channel joins and leaves to the associated text channel",
     color: "#7289da",
     authors: [{ name: "Bluscream", id: 467777925790564352n }, { name: "Cursor.AI", id: 0n }],
